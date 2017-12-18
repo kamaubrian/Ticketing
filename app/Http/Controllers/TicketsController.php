22a169f7f7e882e3cbbc29f2c6a2ba5569e94618
mtotodev05 <mtotodev05@gmail.com>
2017-12-18 22:56:12 +0300

    Integrated Tickets

--- app/Http/Controllers/TicketsController.php
+++ app/Http/Controllers/TicketsController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 use App\Category;
 use App\Ticket;
 use App\Mailers\AppMailer;
-
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class TicketsController extends Controller
