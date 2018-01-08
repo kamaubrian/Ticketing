ea02d7b21931b63f9ea326d93bdcb5aa8219e40b
mtotodev05 <mtotodev05@gmail.com>
2018-01-08 14:51:37 +0300

    Fixed Bugs in Middleware

--- app/Http/Middleware/AdminMiddleWare.php
+++ app/Http/Middleware/AdminMiddleWare.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Support\Facades\Auth;
 
 class AdminMiddleWare
 {
@@ -15,7 +16,7 @@ class AdminMiddleWare
      */
     public function handle($request, Closure $next)
     {
-        if(Auth::user()->is_admin!==1){
+        if(Auth::user()->is_admin!==0){
             return redirect('home');
         }
         return $next($request);
